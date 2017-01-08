07c2273765eacbe14a1d34e1f823819b76b526dd
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-01-08 21:33:57 +0700

    4.17

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,105 @@
-use std::cell::RefCell;
+use std::f64;
 
-struct Point
+struct Circle
 {
-  x: i32,
-  y: i32
+  x: f64,
+  y: f64,
+  radius: f64
 }
 
-struct Point3d
-{
-  x: i32,
-  y: i32,
-  z: i32
+impl Circle {
+  fn new(x: f64, y: f64, radius: f64) -> Circle {
+    Circle {x: x, y: y, radius: radius}
+  }
 }
 
-struct PointRef<'a>
-{
-  x: &'a mut i32,
-  y: &'a mut i32
+impl Circle {
+  fn area(&self) -> f64 {
+    // let mut area =
+    std::f64::consts::PI * (self.radius * self.radius)
+    // Circle {area: area, .. self}
+  }
 }
 
-enum Message
+impl Circle {
+  fn dist_from_origin(&self) -> f64 {
+    ( (self.x * self.x) + (self.y + self.y) ).sqrt()
+  }
+}
+
+impl Circle {
+  fn grow(&self, increment: f64) -> Circle {
+    Circle {x: self.x, y: self.y, radius: self.radius + increment}
+  }
+}
+
+
+struct CircleBuilder
 {
-  Quit,
-  ChangeColor(u32, u32, u32),
-  Write(String),
-  Move { x: i32, y: i32 },
+  x: f64,
+  y: f64,
+  radius: f64
+}
+
+impl CircleBuilder {
+  fn new() -> CircleBuilder {
+    CircleBuilder {x: 0.0, y: 0.0, radius: 1.0}
+  }
+
+  fn x(&mut self, x: f64) -> &mut CircleBuilder {
+    self.x = x;
+    self
+  }
+
+  fn y(&mut self, y: f64) -> &mut CircleBuilder {
+    self.y = y;
+    self
+  }
+
+  fn radius(&mut self, radius: f64) -> &mut CircleBuilder {
+    self.radius = radius;
+    self
+  }
+
+  fn finalize(&self) -> Circle {
+    Circle {x: self.x, y: self.y, radius: self.radius}
+  }
 }
 
 fn main()
 {
-  let mut x = 5;
-  println!("{}", x);
+  let c = Circle::new(2.0, 3.0, 2.0);
+  println!("{}", c.area());
+  println!("{}", c.grow(10.0).area());
 
-  x = 6;
-  println!("{}", x);
+  let c2 = CircleBuilder::new()
+    .x(1.0)
+    .y(2.0)
+    .radius(2.0)
+    .finalize();
 
-  let y = &mut x;
-  // let k = 10;
-  // y = &k;
-  *y += 1;
-  println!("{}", y);
+  println!("{}", c2.area());
 
 
-  let x = RefCell::new(42);
 
-  let y = x.borrow_mut();
-  // let z = x.borrow_mut();
+  let gr = "Hello \
+    there"; // &'static str
+  println!("{}", gr);
 
-  let mut origin = Point { x:0, y: 0 };
-  origin.x = 5;
-  println!("{}, {}", origin.x, origin.y);
+  let mut st = gr.to_string();
+  st.push_str(" world");
+  println!("{}", st);
 
-  let base_point = Point3d { x: 3, y: 8, z: 0 };
-  let point = Point3d { z: 10, .. base_point };
-  println!("{}, {}, {}", point.x, point.y, point.z);
+  let hachiko = "忠犬ハチ公";
 
-  {
-    let r = PointRef { x: &mut 10, y: &mut 10 };
-
-    *r.x = 1;
-    *r.y = 11;
-
-    println!("{}, {}", r.x, r.y);
+  for b in hachiko.as_bytes() {
+      print!("{}, ", b);
   }
 
+  println!("\n{:?}", hachiko.chars().nth(3));
 
-  let x: Message = Message::ChangeColor(3, 5, 6);
+  let y = "Hello, ".to_string();
+  let y2 = " world".to_string();
 
-
-  let x = 5;
-
-  match x {
-    1 => println!("one"),
-    2 => println!("two"),
-    3 => println!("three"),
-    4 => println!("four"),
-    5 => println!("five"),
-    _ => println!("something else"),
-  }
-
-  process_message(Message::ChangeColor(10, 145, 200));
-}
-
-fn process_message(msg: Message)
-{
-  match msg
-  {
-    Message::Quit => quit(),
-    Message::ChangeColor(r, g, b) => change_color(r, g, b),
-    Message::Write(s) => println!("{}", s),
-    _ => println!("messed up"),
-  };
-}
-
-fn quit()
-{
-  println!("quit");
-}
-
-fn change_color(r: u32, g: u32, b: u32)
-{
-  println!("{}, {}, {}", r, g, b);
+  println!("{}", y + &y2);
 }
